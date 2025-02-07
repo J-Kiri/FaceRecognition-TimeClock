e54dcb373070f4bf8346733afba50ecd71db5d52
Joao Gualberto <joaogufilho@gmail.com>
2025-02-07 09:34:03 -0300

    Enviroment Configuration + First Commit

--- EPI_Reco.py
+++ EPI_Reco.py
@@ -1,6 +1,11 @@
 import cv2 as cv
+import pafy
 
-capture = cv2.VideoCapture("")
+url = "https://www.youtube.com/watch?v=ID-yWIleGAM"
+video = pafy.new(url)
+best = video.getbest(preftype="mp4")
+
+capture = cv.VideoCapture(best.url)
 
 while True:
-    ret, frame = capture.read()
+    grabbed, frame = capture.read()
